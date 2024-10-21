2f4239b1f88a9100bbb56cc9fddf9e0dd23a62e8
AmineFjr <fajry599@gmail.com>
2024-10-21 14:55:08 +0200

    feat: add post

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,6 +11,8 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private static List<WeatherForecast> DataWeatherForecasts = new List<WeatherForecast>();
+
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -21,12 +23,21 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        }).ToArray();
+
+        DataWeatherForecasts.AddRange(forecasts);
+        return DataWeatherForecasts;
+    }
+    
+    [HttpPost(Name = "PostWeatherForecast")]
+    public IEnumerable<WeatherForecast> Post([FromBody] WeatherForecast forecast)
+    {
+        DataWeatherForecasts.Add(forecast);
+        return DataWeatherForecasts;
     }
 }
