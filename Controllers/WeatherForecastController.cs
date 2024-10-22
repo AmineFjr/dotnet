2a2543ccb3c8680712b38c190639eca8710cb0d4
AmineFjr <fajry599@gmail.com>
2024-10-22 12:11:36 +0200

    feat: add put and patch

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -69,4 +69,42 @@ public class WeatherForecastController(ILogger<WeatherForecastController> logger
         DataWeatherForecasts.Remove(forecast);
         return Ok(DataWeatherForecasts);
     }
+    
+    [HttpPut("{summary}", Name = "PutWeatherForecast")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<WeatherForecast>> Put(string summary, [FromBody] WeatherForecast forecast)
+    {
+        var forecastOld = DataWeatherForecasts.FirstOrDefault(x => x.Summary == summary);
+        
+        if (forecastOld == null)
+        {
+            return NotFound("Summary not found");
+        }
+        
+        DataWeatherForecasts.Remove(forecastOld);
+        DataWeatherForecasts.Add(forecast);
+        return Ok(DataWeatherForecasts);
+    }
+    
+    [HttpPatch("{summary}", Name = "PatchWeatherForecast")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<WeatherForecast>> Patch(string summary, [FromBody] WeatherForecast forecast)
+    {
+        var forecastOld = DataWeatherForecasts.FirstOrDefault(x => x.Summary == summary);
+        
+        if (forecastOld == null)
+        {
+            return NotFound("Summary not found");
+        }
+        
+        forecastOld.Date = forecast.Date;
+        forecastOld.TemperatureC = forecast.TemperatureC;
+        forecastOld.Summary = forecast.Summary;
+        
+        return Ok(DataWeatherForecasts);
+    }
 }
