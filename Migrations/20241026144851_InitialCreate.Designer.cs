e3dcd4150b2fdb260f22bfb2577850d4e64be6ab
AmineFjr <fajry599@gmail.com>
2024-10-26 19:58:51 +0200

    refacto: movie

--- Migrations/20241026144851_InitialCreate.Designer.cs
+++ Migrations/20241026144851_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorApp.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20241025105820_InitialCreate")]
+    [Migration("20241026144851_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
